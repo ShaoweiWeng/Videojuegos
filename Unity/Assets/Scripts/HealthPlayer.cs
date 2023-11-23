8c7e47855dabdcf2acae178e7748d5bdc2e154c9
Shaowei Weng <shaoweiweng@MacBook-Air-de-Shaowei.local>
2023-11-23 13:39:17 +0100

    Curacion de vida

--- Unity/Assets/Scripts/HealthPlayer.cs
+++ Unity/Assets/Scripts/HealthPlayer.cs
@@ -9,6 +9,9 @@ public class HealthPlayer : MonoBehaviour
     [SerializeField] private int maxHealth = 5;
     [SerializeField] private int currentHealth;
     [SerializeField] private float invincibilityTime = 1.5f;
+    [SerializeField] private float healTime = 30f;
+
+    private Coroutine m_MyCoroutineReference;
     private bool invencible;
 
     //-.-.-ATRIBUTOS - SPRITES-.-.-
@@ -32,25 +35,29 @@ public class HealthPlayer : MonoBehaviour
         createHealthBar();
         invencible = false;
         rb = GetComponent<Rigidbody2D>();
+        m_MyCoroutineReference = StartCoroutine(heal());
     }
 
 
     //Estas funciones se llaman en el objeto que hace daño al jugador
     public void takeDamagePlayer(int damage)
     {
-        if (!invencible && currentHealth>0 )
+        if (!invencible && currentHealth > 0)
         {
+            StopCoroutine(m_MyCoroutineReference);
+            m_MyCoroutineReference = StartCoroutine(heal());
             invencible = true;
             currentHealth = currentHealth - damage;
-            if( currentHealth <= 0)
+            if (currentHealth <= 0)
             {
                 currentHealth = 0;
                 MuerteJugador?.Invoke(this, EventArgs.Empty);//Para crear un evento
-                
+
                 Destroy(gameObject);
                 //AÑADIR ANIMACIONES DE MUERTE + GAMEOVER ETC
             }
-            else{
+            else
+            {
                 StartCoroutine(invincibility());
             }
         }
@@ -60,7 +67,7 @@ public class HealthPlayer : MonoBehaviour
     public void healDamagePlayer(int heal)
     {
         currentHealth = currentHealth + heal;
-        if(currentHealth > maxHealth)
+        if (currentHealth > maxHealth)
         {
             currentHealth = maxHealth;
         }
@@ -70,9 +77,9 @@ public class HealthPlayer : MonoBehaviour
     //Añade una barra de vida con tantos corazones como maxHealth
     private void createHealthBar()
     {
-        for (int i=0; i<heartsInHealthBar.Length; i++)
+        for (int i = 0; i < heartsInHealthBar.Length; i++)
         {
-            if(i < maxHealth)
+            if (i < maxHealth)
             {
                 heartsInHealthBar[i].enabled = true;
             }
@@ -86,9 +93,9 @@ public class HealthPlayer : MonoBehaviour
     // Actualiza la barra de vida, cambiando los corazones por corazones vacios o llenos dependiendo de la vida actual
     private void updateHealthBar()
     {
-        for (int i=0; i<heartsInHealthBar.Length; i++)
+        for (int i = 0; i < heartsInHealthBar.Length; i++)
         {
-            if(i < currentHealth)
+            if (i < currentHealth)
             {
                 heartsInHealthBar[i].sprite = fullHeart;
             }
@@ -100,22 +107,25 @@ public class HealthPlayer : MonoBehaviour
     }
 
     //EL JUGADOR HA TOCADO A UN ENEMIGO
-    private void OnCollisionEnter2D(Collision2D other){
-        if(other.gameObject.CompareTag("Enemy") && !invencible){
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && !invencible)
+        {
 
-            GetComponent<LogicaPlayer>().enKnockb=true;
+            GetComponent<LogicaPlayer>().enKnockb = true;
 
-            if(GetComponent<LogicaPlayer>().isFacingLeft){  //APLICAMOS KNOCKBACK DEPENDIENDO DE LA DIRECCIÓN
-                direction = new Vector2 (4, 2);
+            if (GetComponent<LogicaPlayer>().isFacingLeft)
+            {  //APLICAMOS KNOCKBACK DEPENDIENDO DE LA DIRECCIÓN
+                direction = new Vector2(4, 2);
                 rb.AddForce(direction * force);
             }
-            else{
-                direction = new Vector2 (-4, 2);
+            else
+            {
+                direction = new Vector2(-4, 2);
                 rb.AddForce(direction * force);
             }
 
             takeDamagePlayer(1);
-
             StartCoroutine(knockback());
         }
     }
@@ -132,5 +142,14 @@ public class HealthPlayer : MonoBehaviour
     {
         yield return new WaitForSeconds(knockbackTime);
         GetComponent<LogicaPlayer>().enKnockb = false;
+    }
+
+    private IEnumerator heal()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(healTime);
+            healDamagePlayer(1);
+        }
     }
 }
