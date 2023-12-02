291fdf3508a9e938c5f6c00f2e84962aa4ef7471
TanuMar <mariaaak03@gmail.com>
2023-12-02 18:01:13 +0100

    Cambio de color cuando enemigo es golpeado

--- Unity/Assets/Scripts/VidaEnemigo.cs
+++ Unity/Assets/Scripts/VidaEnemigo.cs
@@ -14,9 +14,18 @@ public class VidaEnemigo : MonoBehaviour
     public bool daKnockbackUp = true;   // True si al hacer un ataque hacia abajo, el jugador rebota de este objeto
     private bool yaGolpeado;    // True si este objeto acaba de ser atacado
 
+    //-.-.-ATRIBUTOS - CAMBIO DE COLOR-.-.-
+    SpriteRenderer mySprite;
+    Color oldColor;
+    Color colorGolpe;
+
     void Start()
     {
         vidaActual = vidaTotal;
+
+        mySprite = this.GetComponent<SpriteRenderer>();
+        oldColor = mySprite.color;
+        colorGolpe = new Color(1, 0, 0, 1);
     }
 
     public void GenerarDaño(int cantidad)
@@ -38,7 +47,9 @@ public class VidaEnemigo : MonoBehaviour
 
     private IEnumerator terminaGolpe()
     {
+        mySprite.color = colorGolpe; //Cuando es golpeado, cambia de color brevemente
         yield return new WaitForSeconds(tiempoInvulnerabilidad);    // Esperar tiempo de invlunerabilidad
+        mySprite.color = oldColor; //recupera su color original
         yaGolpeado = false; // Puede ser golpeado otra vez
     }
 }
