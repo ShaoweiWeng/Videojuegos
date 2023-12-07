4c68a16106e803e5bf94bf7195389fd74833cd0a
Shaowei Weng <shaoweiweng@Air-de-Shaowei.home>
2023-12-07 23:35:49 +0100

    Arreglando herencia de sonidos

--- Unity/Assets/Scripts/ManagerSoundMixer.cs
+++ Unity/Assets/Scripts/ManagerSoundMixer.cs
@@ -2,10 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class ManagerSoundMixer : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+
+    [SerializeField] private Slider barraMaster;
+    [SerializeField] private Slider barraMusic;
+    [SerializeField] private Slider barraEffect;
+
+    private void Awake()
+    {
+        float master, music, effect;
+        if (audioMixer.GetFloat("masterVolume", out master) && master != 0) barraMaster.value = Mathf.Pow(10, master / 20f);
+        if (audioMixer.GetFloat("musicVolume", out music) && music != 0) barraMusic.value = Mathf.Pow(10, music / 20f);
+        if (audioMixer.GetFloat("soundFXVolume", out effect) && effect != 0) barraEffect.value = Mathf.Pow(10, effect / 20f);
+    }
 
     public void SetMasterVolume(float n)
     {
