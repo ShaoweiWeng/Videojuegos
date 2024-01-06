9c7c2c32f608f1c2fab815d0c9765a12b2b610fd
Edda <edda.blanco@alumnos.upm.es>
2024-01-06 23:35:57 +0100

    Creditos

---
+++ Unity/Assets/Scripts/Final.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Final : MonoBehaviour
+{
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Cerradoo");
+    }
+
+}
